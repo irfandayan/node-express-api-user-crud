faa45d6f7262505549552d92419c194d8f43c46d
irfan dayan <engr.irfandayan@gmail.com>
2022-03-14 15:57:43 +0500

    Add get & post routes

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import express from "express";
+import usersRoutes from "./routes/users.js";
 
 const app = express();
 const PORT = 5000;
 
 app.use(express.json());
+app.use("/users", usersRoutes);
 
 app.get("/", (req, res) => res.send("Hello from home page"));
 
