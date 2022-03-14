015297d4d065ef1e0cd25f5d0a56ba59f88b084f
irfan dayan <engr.irfandayan@gmail.com>
2022-03-14 17:58:30 +0500

    Add get post and delete routed

--- routes/users.js
+++ routes/users.js
@@ -1,19 +1,9 @@
 import express from "express";
+import { v4 as uuidv4 } from "uuid";
 
 const router = express.Router();
 
-const users = [
-  {
-    firstName: "John",
-    lastName: "Doe",
-    age: 25,
-  },
-  {
-    firstName: "Jane",
-    lastName: "Doe",
-    age: 24,
-  },
-];
+let users = [];
 
 // All routes in here are starting with /users
 router.get("/", (req, res) => {
@@ -23,9 +13,28 @@ router.get("/", (req, res) => {
 
 router.post("/", (req, res) => {
   const user = req.body;
-  users.push(user);
 
+  // const userId = uuidv4(); // ⇨ '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d'
+  // const userWithId = { ...user, id: uuidv4() };
+
+  users.push({ ...user, id: uuidv4() });
   res.send(`User with the name ${user.firstName} added to the database!`);
+});
+
+router.get("/:id", (req, res) => {
+  console.log(req.params.id);
+  const { id } = req.params;
+
+  const foundUser = users.find((user) => user.id === id);
+
+  res.send(foundUser);
+});
+
+router.delete("/:id", (req, res) => {
+  const { id } = req.params;
+
+  users = users.filter((user) => user.id != id);
+  res.send(`User with the id ${id} deleted from the database.`);
 });
 
 export default router;
