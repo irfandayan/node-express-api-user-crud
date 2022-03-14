ca21a2bd970c39ecef118cc26bbe8ec28cd68197
irfan dayan <engr.irfandayan@gmail.com>
2022-03-14 22:12:25 +0500

    Finished

--- routes/users.js
+++ routes/users.js
@@ -12,12 +12,9 @@ const router = express.Router();
 
 // All routes in here are starting with /users
 router.get("/", getUsers);
-
 router.post("/", createUser);
-
 router.get("/:id", getUser);
-
 router.delete("/:id", deleteUser);
-
 router.patch("/:id", updateUser);
+
 export default router;
